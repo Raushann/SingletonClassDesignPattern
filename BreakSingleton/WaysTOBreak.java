b655a6f96d6729fb214569cb4a631e3a314a3632
Raushan Kumar Singh <50729536+Raushann@users.noreply.github.com>
2023-08-31 18:06:07 +0530

    Update WaysTOBreak.java

--- BreakSingleton/WaysTOBreak.java
+++ BreakSingleton/WaysTOBreak.java
@@ -11,17 +11,17 @@ public class WaysTOBreak {
     public static void main(String[] args) throws Exception {
         Singleton originalSingletonInstance =  Singleton.getInstance();
         Singleton duplicateSingletonInstance =  Singleton.getInstance();
+        
         System.out.println("Hash Code for originalSingletonInstance is : "+ originalSingletonInstance.hashCode());
         System.out.println("Hash Code for duplicateSingletonInstance is : "+ duplicateSingletonInstance.hashCode());
 
         System.out.println("---------Breaking Using Reflection---------");
 
-
-
         // Reflection
+        
         Class <?> singletonClass = Class.forName("SingletonPractice.Singleton");
         Constructor<Singleton> constructor = (Constructor<Singleton>) singletonClass.getDeclaredConstructor();
-
+        
         constructor.setAccessible(true);
         Singleton brokenSingletonUsingReflection = constructor.newInstance();
         System.out.println("Hash Code for brokenSingletonUsingReflection is : "+ brokenSingletonUsingReflection.hashCode());
@@ -29,11 +29,13 @@ public class WaysTOBreak {
         System.out.println("---------Breaking Using Serialization---------");
 
         //Serialization
+        
         ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream("/Users/raushankumar/Downloads/Serialization.ser"));
         objectOutputStream.writeObject(originalSingletonInstance);
         objectOutputStream.close();
 
         //deserialization
+        
         ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream("/Users/raushankumar/Downloads/Serialization.ser"));
         Singleton brokenSingletonUsingSerialization = (Singleton) objectInputStream.readObject();
 
